e51463237f82e80852c37ec3da1376a580a280b7
Edanriell <Edanriell@gmail.com>
2025-11-02 11:57:40 +0200

    another project version

---
+++ src/Bookify.Infrastructure/Authorization/CustomClaimsTransformation.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Claims;
+using Bookify.Domain.Users;
+using Bookify.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.JsonWebTokens;
+
+namespace Bookify.Infrastructure.Authorization;
+
+internal sealed class CustomClaimsTransformation : IClaimsTransformation
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public CustomClaimsTransformation(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+    {
+        bool hasRoleClaim = principal.HasClaim(c => c.Type == ClaimTypes.Role);
+        bool hasSubClaim = principal.HasClaim(c => c.Type == JwtRegisteredClaimNames.Sub);
+
+        bool isUnauthenticated = principal.Identity is not { IsAuthenticated: true };
+        bool hasBothClaims = hasRoleClaim && hasSubClaim;
+
+        if (isUnauthenticated || hasBothClaims)
+        {
+            return principal;
+        }
+
+        using IServiceScope scope = _serviceProvider.CreateScope();
+
+        AuthorizationService authorizationService = scope.ServiceProvider.GetRequiredService<AuthorizationService>();
+
+        string identityId = principal.GetIdentityId();
+
+        UserRolesResponse userRoles = await authorizationService.GetRolesForUserAsync(identityId);
+
+        var claimsIdentity = new ClaimsIdentity();
+
+        claimsIdentity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub, userRoles.UserId.ToString()));
+
+        foreach (Role role in userRoles.Roles)
+        {
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, role.Name));
+        }
+
+        principal.AddIdentity(claimsIdentity);
+
+        return principal;
+    }
+}
